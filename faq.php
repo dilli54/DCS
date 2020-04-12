c157134a89647c00f9d562e2ae98c018798e2003
dilli54 <48171934+dilli54@users.noreply.github.com>
2020-04-12 21:12:01 +0530

    Add files via upload

---
+++ faq.php
@@ -0,0 +1,139 @@
+<html>
+<head>
+        <title>FAQ | DCS</title>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<!--<script src="js/jquery.min.js"></script>-->
+<script src="js/jquery-1.8.3.js"></script>
+ <!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+ <!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!----webfonts--->
+<!--<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css' />-->
+<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,900italic,900,700italic,700,500italic,500,400italic' rel='stylesheet' type='text/css' />
+<!---//webfonts--->
+    </head>
+<body>
+    <!---- container ---->
+<!---- header ----->
+<div class="header  about-head "  >
+        <div class="container">
+                <!--- logo ----->
+                <div class="logo">
+                    <img src="images/logo45.png" alt="Logo"  /> <a href="index.php"><span></span>DCS</a>
+                </div>
+                <!--- logo ----->
+<!--- top-nav ----->
+<div class="top-nav">
+    <span class="menu"> </span>
+    <ul>
+        <li ><a href="index.php">Home</a></li>
+        <!--<li class="active" ><a href="faq.php">FAQ</a></li>-->
+        <li ><a href="contact.php">Contact</a></li>
+                <li ><a href="login.php">Login</a></li>
+        <li ><a href="register.php">Register</a></li>
+            </ul>
+</div>
+<div class="clearfix"> </div>
+<!--- top-nav ----->
+        <!----- script-for-nav ---->
+<script>
+        $( "span.menu" ).click(function() {
+          $( ".top-nav ul" ).slideToggle( "slow", function() {
+            // Animation complete.
+          });
+        });
+</script>
+<!----- script-for-nav ---->
+        </div>
+    </div>
+<!---- header ----->
+<!------ about ---->
+<div class="about">
+    <!--- bradcrumbs ---->
+    <div class="breadcrumbs">
+        <div class="container">
+            <div class="breadcrumbs-left">
+                <h1>FAQ</h1>
+            </div>
+            <div class="breadcrumbs-right">
+                <ul>
+                    <li><a href="index.php">Home</a> <label>:</label></li>
+                    <li>FAQ</li>
+                </ul>
+            </div>
+            <div class="clearfix"> </div>
+        </div>
+    </div>
+    <!--- bradcrumbs ---->
+</div>
+<div class="about-top-grids">
+    <div class="container">
+        
+        <!---- about-grids ---->
+        <div class="about-grids">
+            <div class="about-grids-row1">
+                                <p>
+                                    Become a customer to get items delivered, become an employee to deliver items.
+                                    <br>
+                                    <br>
+                                    No FAQs since the interface is simple.
+                                </p>            </div>
+        </div>
+        <!---- about-grids ---->
+        
+    </div>
+<!------ about ---->
+</div>
+
+<div class="footer">
+    <div class="top-footer">
+            <div class="container">
+                    <div class="top-footer-grids">
+                            <div class="top-footer-grid">
+                                    <h3>Contact us</h3>
+                                    <ul class="address">
+                                        <li><span class="map-pin"> </span><label>T.N Nagar<br>Palanipet <br>Near railway station, Arakkonam TN (631002) </label></li>
+                                        <li><span class="mob"> </span>Ph & Fax no - 044-225650, Mob- 8056320993</li>
+                                        <li><span class="msg"> </span><a href="#">hello@dcs.in</a></li>
+                                    </ul>
+                            </div>
+                            <div class="top-footer-grid">
+                                    <h3>Important Links</h3>
+                                    <ul class="latest-post">
+                                        <li><a href="index.php">Home</a> </li>
+                                         
+                                        <li><a href="register.php">Register</a> </li>
+                                        <li><a href="login.php">Login</a> </li>
+                                    </ul>
+                            </div>
+                            <div class="top-footer-grid">
+                                    <h3>Other Links</h3>
+                                    <ul class="latest-post">
+                                        <li><a href="about-us.php">About Us</a> </li>
+                                        <li><a href="privacy-policy.php">Privacy Policy</a> </li>
+                                        <li><a href="terms-and-condition.php">Terms & Conditions</a> </li>
+                                        <li><a href="faq.php">Help & FAQs</a> </li>
+                                        <li><a href="contact.php">Contact Us</a> </li>
+                                    </ul>
+                            </div>
+                            <div class="clear"> </div>
+                    </div>
+            </div>
+    </div>
+    <!----start-bottom-footer---->
+    <div class="bottom-footer">
+            <div class="container"> 
+                    <div class="bottom-footer-left">
+                        
+                             <p> &copy; 2017 DCS.in. All rights reserved | Powered by: <a href="https://www.facebook.com/profile.php?id=100012111486045" target="_blank">Hacker Technologies</a></p>
+                    </div>
+                    <div class="clear"> </div>
+            </div>
+    </div>
+    <!----//End-bottom-footer---->
+</div>
+	</body>
+</html>
